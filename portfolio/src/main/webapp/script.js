565ee7bf72b314f7e72e71548d0e6eb9392419cd
Alisa <alisalai2011@gmail.com>
2020-06-08 19:43:31 +0000

    Make canvas and context local variables and add/fix constant names

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -35,16 +35,21 @@ function addRandomFact() {
   factContainer.innerText = fact; 
 }
 
-const size = 30;
-var canvas;
-var ctx;
+const SNAKE_SQUARE_SIZE = 30;
 var x = new Array(5);
 var y = new Array(5);
 
+function getCanvasAndContext() {
+  var canvas = document.getElementById('snakeCanvas'); // Get the canvas element by Id
+  var ctx = canvas.getContext('2d'); // Canvas 2d rendering context
+  return {
+    canvas: canvas,
+    ctx: ctx,
+  };
+}
+
 function initSnake() {
-  canvas = document.getElementById('snakeCanvas'); // Get the canvas element by Id
-  ctx = canvas.getContext('2d'); // Canvas 2d rendering context
-  for (var z=0; z < 5; z++) {
+  for (var z = 0; z < 5; z++) {
     x[z] = 10;
     y[z] = 10;
   }
@@ -52,12 +57,16 @@ function initSnake() {
 }
 	
 function drawSnake() {
+  var values = getCanvasAndContext();
+  var canvas = values.canvas;
+  var ctx = values.ctx;
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   
-  for (var z=0; z<5; z++) {
+  for (var z = 0; z < 5; z++) {
     ctx.beginPath();
-    ctx.rect(x[z], y[z], size, size);
-    ctx.fillStyle = "#FF0000";
+    ctx.rect(x[z], y[z], SNAKE_SQUARE_SIZE, SNAKE_SQUARE_SIZE);
+    ctx.fillStyle = "#ff0000";
     ctx.fill();
     ctx.closePath();
   }
@@ -68,28 +77,30 @@ const RIGHT_KEY = 39;
 const UP_KEY = 38;
 const DOWN_KEY = 40;
 
-//move snake inside the canvas using arrow keys
+const STEP_SNAKE = 20;
+
+// move snake inside the canvas using arrow keys
 window.onkeydown = function(event) {
   for (var z = 4; z > 0; z--) {
     x[z] = x[(z - 1)];
     y[z] = y[(z - 1)];
   }
+
+  var values = getCanvasAndContext();
+  var canvas = values.canvas;
   
-  var keyPr = event.keyCode; //Key code of key pressed
+  var keyPr = event.keyCode; // Key code of key pressed
   
-  if(keyPr == RIGHT_KEY && x[0]<=canvas.width - 40) { 
-    x[0] = x[0]+20;
-  }
-  else if(keyPr == LEFT_KEY && x[0]>10) {
-    x[0] = x[0]-20; 
-  }
-  else if(keyPr == UP_KEY && y[0]>10) {
-    y[0] = y[0]-20; 
-  }
-  else if(keyPr == DOWN_KEY && y[0]<=canvas.height - 40) {
-    y[0] = y[0]+20; 
+  if(keyPr == RIGHT_KEY && x[0] <= canvas.width - 40) { 
+    x[0] = x[0] + STEP_SNAKE;
+  } else if(keyPr == LEFT_KEY && x[0] > 10) {
+    x[0] = x[0] - STEP_SNAKE; 
+  } else if(keyPr == UP_KEY && y[0] > 10) {
+    y[0] = y[0] - STEP_SNAKE; 
+  } else if(keyPr == DOWN_KEY && y[0] <= canvas.height - 40) {
+    y[0] = y[0] + STEP_SNAKE; 
   }
   
-  //Drawing snake at new position
+  // Drawing snake at new position
   drawSnake();
 };
